b1ada365d68d73235dcc69d5a9f0bb25157756aa
avasyukhin <avasyukhin@inbox.ru>
2016-05-05 08:19:05 +0400

    printArray with iterator

--- src/main/java/RandArray.java
+++ src/main/java/RandArray.java
@@ -1,22 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Random;
 
 /**
  * Created by Aphex on 03.05.2016.
  */
 public class RandArray {
-    public static int[] initArray(int ARRAY_LENGTH) {
-        int[] array = new int[ARRAY_LENGTH];
+    public static int[] initArray(int array_length) {
+        int[] array = new int[array_length];
         Random generator = new Random();
-        for (int i=0; i<ARRAY_LENGTH; i++) {
+        for (int i=0; i<array_length; i++) {
             array[i] = generator.nextInt();
         }
         return array;
     }
-    public static void printArray(int ARRAY_LENGTH) {
-        int [] array = initArray(ARRAY_LENGTH);
-        for (int i=0; i<ARRAY_LENGTH-1; i++) {
-            System.out.print(array[i] + ", ");
+    public static void printArray(int array_length) {
+        ArrayList array = new ArrayList();
+        Collections.addAll(array, initArray(array_length));
+        Iterator iter = array.iterator();
+        while (iter.hasNext()){
+            System.out.print(iter.next());
+            if (iter.hasNext()){
+                System.out.print(", ");
+            }else {
+                System.out.print("\n");
+            }
+
         }
-        System.out.println(array[ARRAY_LENGTH-1]);
+
+
+
     }
 }
